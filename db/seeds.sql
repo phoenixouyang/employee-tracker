613660b0e8780d29169cdf8ace36a8d6b2d72134
Pholobo <ouyangphoenix@gmail.com>
2023-03-18 17:36:51 -0400

    adds data to seeds.sql to populate tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,52 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
+INSERT INTO departments (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Human Resources"),
+       ("Management");
 
-USE employees_db;
+INSERT INTO roles (title, salary, department_id)
+VALUES ("President", 250000, 6),
+       ("Finance Director", 200000, 6),
+       ("Sales Director", 200000, 6),
+       ("Operations Director", 200000, 6),
+       ("Executive Assistant", 80000, 6),
+       ("Sales Manager", 120000, 1),
+       ("Sales Representative", 95000, 1),
+       ("Lead Engineer", 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Junior Developer", 80000, 2),
+       ("Finance Manager", 120000, 3),
+       ("Financial Analyst", 90000, 3),
+       ("Accountant", 60000, 3),
+       ("Legal Team Lead", 180000, 4),
+       ("Lawyer", 150000, 4),
+       ("Human Resource Manager", 100000, 5),
+       ("Human Resource Associate", 60000, 5);
 
-CREATE TABLE departments (
-    id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-);
 
-CREATE TABLE roles (
-    id INT NOT NULL PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL,
-);
-
-CREATE TABLE employees (
-    id INT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles(id), ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL,
-);
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Phoenix", "Ouyang", 1, NULL),
+       ("David", "Wallace", 2, 1),
+       ("Jan", "Levinson", 3, 1),
+       ("Robert", "California", 4, 1),
+       ("Yoo", "Jae-suk", 5, 1),
+       ("Michael", "Scott", 6, 3),
+       ("Jim", "Halpert", 7, 6),
+       ("Dwight", "Schrute", 7, 6),
+       ("Phyllis", "Lapin-Vance", 7, 6),
+       ("Harry", "Potter", 8, 1),
+       ("Hermione", "Granger", 9, 8),
+       ("Ron", "Weasley", 9, 8),
+       ("Luna", "Lovegood", 10, 8),
+       ("Tanjiro", "Kamado", 11, 2),
+       ("Zenitsu", "Agatsuma", 12, 11),
+       ("Inosuke", "Hashibira", 12, 11)
+       ("Nezuko", "Kamado", 13, 11),
+       ("Joey", "Tribbiani", 14, 4),
+       ("Rachel", "Green", 15, 14),
+       ("Solar", "Kim", 16, 4),
+       ("Moon", "Byul", 17, 16),
+       ("Wheein", "Jung", 17, 16),
+       ("Hwasa", "Ahn", 17, 16);
