616db9b4ba52b194b53419f85604b81bade93d5a
Pholobo <ouyangphoenix@gmail.com>
2023-03-20 16:15:24 -0400

    adds function to add a department to database

--- server.js
+++ server.js
@@ -1,55 +1,102 @@
 const inquirer = require('inquirer');
-const {viewDepts} = require('./lib/lib');
+const {viewDepts, viewRoles, viewEmployees} = require('./lib/viewDB');
+const {addDept, addRole, addEmp} = require('./lib/addDB');
 
-// View all departments
+// Add a department
+const addDeptPrompt = [
+    {
+        type: 'input',
+        name: 'department',
+        message: 'Please enter the name of the new department you would like to add.',
+        validate: departmentInput => {
+            if (departmentInput) {
+                return true;
+            } else {
+                console.log('Please enter a name for your new department.')
+                return false;
+            }
+        }
+    }
+]
 
-async function menu() { 
+async function addDeptFunc() {
+    let newDeptInq = await inquirer.prompt(addDeptPrompt);
+    let newDept = addDept(newDeptInq.department);
+    console.log('New department has been added')
+    returnMenu();
+}
+
+
+// List of prompts for main menu
+const menu = [
+    {
+        type: 'list',
+        name: 'menu',
+        message: 'What would you like to do?',
+        choices: 
+        [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role'
+        ]
+    }
+]
+
+// switch case function for each menu option
+async function startMenu() {
+    let response = await inquirer.prompt(menu);
+    switch(response.menu) {
+        case 'View all departments':
+            viewDepts();
+            setTimeout(returnMenu, 1000);
+            break;
+        case 'View all roles':
+            viewRoles();
+            setTimeout(returnMenu, 1000);
+            break;
+        case 'View all employees':
+            viewEmployees();
+            setTimeout(returnMenu, 1000);
+            break;
+        case 'Add a department':
+            addDeptFunc();
+            break;
+        case 'Add a role':
+            console.log('view roles')
+            break;
+        case 'Add an employee':
+            console.log('view roles')
+            break;
+        case 'Update an employee role':
+            console.log('view roles')
+            break;
+        default:
+            break;
+    }
+
+}
+
+// return function to bring user back to main menu, or exit application
+function returnMenu() {
     inquirer.prompt([
         {
-            type: 'list',
-            name: 'menu',
-            message: 'What would you like to do?',
-            choices: 
-            [
-                'View all departments',
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add an employee',
-                'Update an employee role'
-            ]
+            type: 'confirm',
+            name: 'return',
+            message: 'Return to main menu?',
+
         }
     ])
     .then((data) => {
-        let response = data;
-        switch(response.menu) {
-            case 'View all departments':
-                let departments = viewDepts();
-                console.table(departments);
-                menu();
-                break;
-            case 'View all roles':
-                console.log('view roles')
-                break;
-            case 'View all employees':
-                console.log('view roles')
-                break;
-            case 'Add a department':
-                console.log('view roles')
-                break;
-            case 'Add a role':
-                console.log('view roles')
-                break;
-            case 'Add an employee':
-                console.log('view roles')
-                break;
-            case 'Update an employee role':
-                console.log('view roles')
-                break;
+        if (data.return) {
+            startMenu()
+        } else {
+        process.exit();
         }
-        
     })
-};
+}
 
-menu();
+startMenu();
